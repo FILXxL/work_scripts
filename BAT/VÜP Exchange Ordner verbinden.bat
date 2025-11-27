f4b027f1a6130abbaf69454b116aa98df8b0e30e
FILXxL <73700341+FILXxL@users.noreply.github.com>
2025-11-27 09:33:00 +0100

    try cmdkey for exchange ordner.bat

--- BAT/VÜP Exchange Ordner verbinden.bat
+++ BAT/VÜP Exchange Ordner verbinden.bat
@@ -4,6 +4,8 @@ set exchange_default_buchstabe="Z"
 set /p exchange_buchstabe="Welcher Laufwerksbuchstabe fuer den ExchangeOrdner?:"
 set /p password="Bitte gib das Passwort ein, das du erhalten hast:"
 if %exchange_buchstabe%=="" set %exchange_buchstabe%=%exchange_default_buchstabe%
+
+cmdkey /add:\\10.123.0.41\exchange /user:sd0402206\0402206_parkexchange /pass:%password%
 net use %exchange_buchstabe%: /delete
 net use %exchange_buchstabe%: \\10.123.0.41\exchange %password% /user:sd0402206\0402206_parkexchange /persistent:yes
 echo "ExchangeOrdner als Laufwerk %vexchange_buchstabe% eingerichtet."
